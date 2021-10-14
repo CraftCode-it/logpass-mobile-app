1d59f3e95815f0b3c587c2a855eb2cca6d72b022
Andrzej Nowak <andrzej.nowak@iteo.com>
2021-10-14 12:46:19 +0200

    change UI for pinocode element

--- lib/presentation/widget/pin_field.dart
+++ lib/presentation/widget/pin_field.dart
@@ -15,11 +15,13 @@ class PinField extends HookWidget {
   final Function(String pin) onPinChanged;
   final bool autoFocus;
   final bool showSideIcon;
+  final String? pinErrorText;
 
   const PinField({
     required this.onPinChanged,
     this.autoFocus = true,
     this.showSideIcon = true,
+    this.pinErrorText,
     Key? key,
   }) : super(key: key);
 
@@ -30,37 +32,60 @@ class PinField extends HookWidget {
     final isObscured = useState(true);
 
     return Row(
+      mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const SizedBox(width: AppDimens.l + AppDimens.xs),
         Container(
           constraints: const BoxConstraints(maxWidth: _pinFieldMaxWith),
-          child: PinPut(
-            onChanged: onPinChanged,
-            fieldsCount: appPinLength,
-            fieldsAlignment: MainAxisAlignment.center,
-            autofocus: autoFocus,
-            eachFieldMargin: const EdgeInsets.symmetric(horizontal: AppDimens.s),
-            selectedFieldDecoration: _inputBorder(colors),
-            followingFieldDecoration: _inputBorder(colors),
-            submittedFieldDecoration: _inputBorder(colors),
-            eachFieldPadding: const EdgeInsets.all(AppDimens.m),
-            textStyle: typography.h9,
-            eachFieldWidth: _singlePinFieldWidth,
-            obscureText: isObscured.value ? '*' : null,
+          child: Column(
+            children: [
+              PinPut(
+                onChanged: onPinChanged,
+                fieldsCount: appPinLength,
+                fieldsAlignment: MainAxisAlignment.center,
+                autofocus: autoFocus,
+                eachFieldMargin: const EdgeInsets.symmetric(horizontal: AppDimens.s),
+                selectedFieldDecoration: _inputBorder(colors),
+                followingFieldDecoration: _inputBorder(colors),
+                submittedFieldDecoration: _inputBorder(colors),
+                eachFieldPadding: const EdgeInsets.all(AppDimens.m),
+                textStyle: typography.h9,
+                eachFieldWidth: _singlePinFieldWidth,
+                obscureText: isObscured.value ? '*' : null,
+              ),
+              if (pinErrorText != null) ...[
+                const SizedBox(height: AppDimens.s),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: AppDimens.m),
+                    child: Text(
+                      pinErrorText!,
+                      textAlign: TextAlign.left,
+                      style: typography.input.copyWith(color: AppColors.error100),
+                    ),
+                  ),
+                ),
+              ]
+            ],
           ),
         ),
-        Opacity(
-          opacity: showSideIcon ? 1 : 0,
-          child: IconButton(
-            onPressed: showSideIcon ? () => isObscured.value = !isObscured.value : null,
-            padding: const EdgeInsets.all(AppDimens.xs),
-            constraints: const BoxConstraints(),
-            icon: SvgPicture.asset(
-              isObscured.value ? AppIcon.passwordEyeShow : AppIcon.passwordEyeHide,
-              width: AppDimens.l,
-              height: AppDimens.l,
-              color: colors.text,
+        Padding(
+          padding: const EdgeInsets.only(top: AppDimens.s + AppDimens.xs),
+          child: Opacity(
+            opacity: showSideIcon ? 1 : 0,
+            child: IconButton(
+              onPressed: showSideIcon ? () => isObscured.value = !isObscured.value : null,
+              padding: const EdgeInsets.all(AppDimens.xs),
+              constraints: const BoxConstraints(),
+              icon: SvgPicture.asset(
+                isObscured.value ? AppIcon.passwordEyeShow : AppIcon.passwordEyeHide,
+                width: AppDimens.l,
+                height: AppDimens.l,
+                color: colors.text,
+              ),
             ),
           ),
         ),
@@ -71,7 +96,7 @@ class PinField extends HookWidget {
   BoxDecoration _inputBorder(AppThemeColors colors) {
     return BoxDecoration(
       border: Border.all(
-        color: colors.inputFocusedBorder,
+        color: pinErrorText != null ? AppColors.error100 : colors.inputFocusedBorder,
         width: 1.0,
       ),
     );
